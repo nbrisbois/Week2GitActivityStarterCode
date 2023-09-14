3c717df8e8ff0a9eff8840216afc0b3733dcca78
nbrisbois <brisbois.n@northeastern.edu>
2023-09-14 14:49:04 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { Calculator } from './calculator';
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
-
+const result_divide = calc.divide(4, 2);
 const result_multiply = calc.multiply(2, 2);
+const result_subtract = calc.subtract(3,2);
+
 console.log(`result_add = ${result_add}`);
 console.log(`result_multiply = ${result_multiply}`);
-
-const result_subtract = calc.subtract(3,2);
-console.log(`result_add = ${result_add}`);
 console.log(`result_subtract = ${result_subtract}`);
+console.log(`result_divide = ${result_divide}`);
